d964b67028dda40e7dd5b61f1fff04f2acc4e1fb
A.M. ABHISHEK SAI <83504755+Abhishek-Sai-14@users.noreply.github.com>
2021-08-30 12:35:29 +0530

    Update load_database.m

--- load_database.m
+++ load_database.m
@@ -1,14 +1,16 @@
 
-function output_value = load_database();
-persistent loaded;
-persistent numeric_Image;
-if(isempty(loaded))
-    all_Images = zeros(10304,40);
-    for i=1:40
-        cd(strcat('s',num2str(i)));
-        for j=1:10
-            image_Container = imread(strcat(num2str(j),'.pgm'));
-            all_Images(:,(i-1)*10+j)=reshape(image_Container,size(image_Container,1)*size(image_Container,2),1);
+function output_value = load_database();  % creating a function 
+persistent loaded;  % Persistent variables are similar to global variables like static 
+persistent numeric_Image;  
+if(isempty(loaded))  % If it is empty only then we will load the dataset.
+    all_Images = zeros(8600,40);  % We have 40 images. Each of them have 92 x 112 = 10304 pixels. That is why we have to take 10304 zeros for 40 times but since we resized we have to change
+    for i=1:40        % for folders from 1 to 40 like s1,s2 ....
+        cd(strcat('s',num2str(i))); % The “ strcat ” function ignores trailing whitespace characters in character vectors.
+        for j=1:10 % for images
+            image_Container = imread(strcat(num2str(j),'.pgm')); %  reading the images of pgm extention
+            image_Container=imresize(image_Container,[100 86]); % resized image to 80 percent
+            all_Images(:,(i-1)*10+j)=reshape(image_Container,size(image_Container,1)*size(image_Container,2),1); %   used ‘reshape’ function to convert the images into single column matrix.
+            all_Images(:,(i-1)*10+j)=all_Images(:,(i-1)*10+j)+50; % increasing brightness
         end
         display('Loading Database');
         cd ..
@@ -17,3 +19,4 @@ if(isempty(loaded))
 end
 loaded = 1;
 output_value = numeric_Image;
+
